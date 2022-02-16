3c8ed7dde10248c71dc5ceaa1adcacaf826ea082
princerequino <requino.prince@gmail.com>
2022-02-16 14:41:33 -0500

    es6 short hand implemented

--- play.js
+++ play.js
@@ -1,21 +1,3 @@
-const net = require("net");
+const net = require('net');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '172.28.77.148' ,// IP address here,
-    port: 50541// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+const {connect} = require("./client.js")
